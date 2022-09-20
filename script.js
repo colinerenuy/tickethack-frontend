3d337ec961943e1334024a97f779367a4c028718
Coline Renuy <renuy.coline@gmail.com>
2022-09-20 17:31:58 +0200

    WIP script.js adding addToCart() function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//========================SEARCH FORM===============================
 
 document.querySelector('#search-form').onsubmit = function() {return false;}
 document.querySelector('#search-form').addEventListener('submit', function() {
@@ -10,30 +10,50 @@ document.querySelector('#search-form').addEventListener('submit', function() {
     query.append('arrival', arrival);
     query.append('departure', departure)
     query.append('date', date)
-    console.log({arrival, departure, date});
-    console.log(query.toString());
-    /*const form = document.querySelector("#search-form");
-    const formData = new FormData();
-    formData.append("departure", document.querySelector("search-departure").value)
-    console.log(formData);*/
+    //console.log({arrival, departure, date});
+    //console.log(query.toString());
 
-  //  http://localhost:3000/trips?departure=Paris&arrival=Lyon&date=2022-09-20
-//http://localhost:3000/trips?departure=${arrival}&arrival=${departure}&date=${date}
 document.querySelector('#result-container').innerHTML = '';
 
 fetch(`http://localhost:3000/trips?${query.toString()}`)
 .then(response => response.json())
  .then(data => {
-    console.log(data);
-    console.log(data.trips);
+    //console.log(data);
+    //console.log(data.trips);
     for (obj of data.trips) {
         document.querySelector('#result-container').innerHTML += `
     <div class = "trip-container">
         <div class = "trip-searched">${obj.departure} > ${obj.arrival}</div>
         <div class = "trip-time"></div>
         <div class = 'trip-price'>${obj.price}€</div>
-        <button type = "submit" class = 'btn-book' type = "button">Book</button>
+        <button type = "button" class = 'btn-book' type = "button">Book</button>
     </div>
     `
+    addToCart()
  }});
+
+
 })
+
+//========================ADD TO CART===============================
+
+function addToCart() {
+
+document.querySelectorAll('.btn-book').onsubmit = function() {return false;}
+
+const buttons = document.querySelectorAll('.btn-book');
+
+for (let i = 0 ; i<buttons.length ; i++) {
+
+    buttons[i].addEventListener('click', function() {
+        const tripSearched = document.querySelectorAll('.trip-searched')[i].textContent;
+        const tripPrice = document.querySelectorAll('.trip-price')[i].textContent;
+        const tripTime = document.querySelectorAll('.trip-time')[i].textContent;
+        
+        window.location.assign('cart.html');
+        
+        
+    })
+}
+
+}
