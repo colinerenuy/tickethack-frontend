1216497a012768b89a7a687c358e108f27afab50
Coline Renuy <renuy.coline@gmail.com>
2022-09-21 11:46:36 +0200

    function addtocart added

--- cart.js
+++ cart.js
@@ -6,22 +6,10 @@ function deletetrip(){
             this.parentNode.remove();
         });
     
-        // if ( document.querySelectorAll('.btn-delete').length == 0){
-        //     console.log('hello')
-        //     console.log( document.querySelectorAll('.btn-delete').length)
-        //     document.querySelector(".total-trip").style.display = none;
-            
-    
-        //     // document.querySelector('.container-info-trip').innerHTML += `
-        //     // <div class="info-trip">
-        //     //     <div class="no-found">
-        //     //         <p>No tickets in your cart.</p>
-        //     //         <p>Why not plan a trip ?</p>
-        //     //     </div>   
-        //     // </div>`
-        // }
     }
    
 
 }
 deletetrip()
+
+
--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+//=========================GLOBAL VARIABLES=========================
+ let date = '';
+
 //========================SEARCH FORM===============================
 
 document.querySelector('#search-form').onsubmit = function() {return false;}
 document.querySelector('#search-form').addEventListener('submit', function() {
     
-    const arrival = document.querySelector('#search_departure').value;
-    const departure = document.querySelector('#search_arrival').value;
-    const date = document.querySelector('#search_date').value;
+    
+    const arrival = document.querySelector('#search_arrival').value;
+    const departure = document.querySelector('#search_departure').value;
+    date = document.querySelector('#search_date').value;
     let query = new URLSearchParams();
-    query.append('arrival', arrival);
-    query.append('departure', departure)
-    query.append('date', date)
+    query.append('departure', departure);
+    query.append('arrival', arrival); 
+    query.append('date', date);
     //console.log({arrival, departure, date});
     //console.log(query.toString());
-
-document.querySelector('#result-container').innerHTML = '';
+    document.querySelector('#result-container').innerHTML = '';
 
 fetch(`http://localhost:3000/trips?${query.toString()}`)
 .then(response => response.json())
@@ -23,38 +26,53 @@ fetch(`http://localhost:3000/trips?${query.toString()}`)
     for (obj of data.trips) {
         document.querySelector('#result-container').innerHTML += `
     <div class = "trip-container">
-        <div class = "trip-searched">${obj.departure} > ${obj.arrival}</div>
+        <div class = "trip-searched"><span class = "departure"> ${obj.departure}</span> > <span class = "arrival">${obj.arrival}</span></div>
         <div class = "trip-time"></div>
         <div class = 'trip-price'>${obj.price}€</div>
         <button type = "button" class = 'btn-book' type = "button">Book</button>
     </div>
     `
     addToCart()
+
  }});
 
 
 })
 
+
 //========================ADD TO CART===============================
 
+
+addToCart()
+
 function addToCart() {
+    document.querySelectorAll('.btn-book').onsubmit = function() {return false;}
 
-document.querySelectorAll('.btn-book').onsubmit = function() {return false;}
+    const buttons = document.querySelectorAll('.btn-book');
 
-const buttons = document.querySelectorAll('.btn-book');
+    for (let i = 0 ; i < buttons.length ; i++) {
 
-for (let i = 0 ; i<buttons.length ; i++) {
+        buttons[i].addEventListener('click', function() {
 
-    buttons[i].addEventListener('click', function() {
-        const tripSearched = document.querySelectorAll('.trip-searched')[i].textContent;
-        const tripPrice = document.querySelectorAll('.trip-price')[i].textContent;
-        const tripTime = document.querySelectorAll('.trip-time')[i].textContent;
-        
-        window.location.assign('cart.html');
+        const departure = document.querySelectorAll('.departure')[i].textContent;
+        const arrival = document.querySelectorAll('.arrival')[i].textContent;
+        const priceEuro = document.querySelectorAll('.trip-price')[i].textContent;
+        const price = Number(priceEuro.replace(/[^0-9.-]+/g,""))
+        console.log(price);
 
-
-        
+        fetch(`http://localhost:3000/cart`, {
+            method: 'POST', 
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ departure, arrival, date, price }),
     })
-}
+        .then(response => response.json())
+        .then(data => { 
+            console.log(data)
+            console.log('added to cart')
+ })
+
+        })
+
+    }
 
 }
